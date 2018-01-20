5848702d067c54a8cc91cc162600339e790a262f
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-01-19 18:07:57 -0600

    :sparkles: adding challenge 5

---
+++ 05 - Flex Panel Gallery/app.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    panels.forEach(panel => panel.classList.remove("open", "open-active"));
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
